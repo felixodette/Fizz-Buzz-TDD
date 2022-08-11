5f229d84888e26d67e8c081ac781fcecad940f6f
Felix Odete <felix.odette@gmail.com>
2022-08-11 08:29:36 +0300

    Add fizzbuzz function

--- solver.rb
+++ solver.rb
@@ -9,4 +9,16 @@ class Solver
       num * factorial(num - 1)
     end
   end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num
+    end
+  end
 end
