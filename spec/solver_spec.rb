d75cdb21d9ccea01f0389312658e9106026b6bfb
Felix Odete <felix.odette@gmail.com>
2022-08-11 07:57:34 +0300

    Code solver test script

---
+++ spec/solver_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require_relative '../solver'
+
+describe 'Solver' do
+  before :each do
+    @solver = Solver.new
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context '#Factoria method' do
+    it 'Should take one argument N and return the factorial of that number' do
+      expect(@solver.factorial(0)).to eql(1)
+    end
+
+    it 'Should return 720 when N is 6' do
+      expect(@solver.factorial(6)).to eql(1)
+    end
+  end
+end
